7dbc9aeae7b861c4f4ce008fffb9b56e5e9a0421
Painezor <Paine.exe@gmail.com>
2020-01-18 16:02:05 +0000

    Despammify TV
    Rewrite embed code, trim down code heavily.

--- ext/tv.py
+++ ext/tv.py
@@ -19,7 +19,7 @@ class Tv(commands.Cog):
 			with open('tv.json',"w",encoding='utf-8') as f:
 				json.dump(self.bot.tv,f,ensure_ascii=True,
 				sort_keys=True,indent=4, separators=(',',':'))
-	
+
 	@commands.command()
 	@commands.is_owner()
 	async def poptv(self,ctx):
@@ -48,141 +48,127 @@ class Tv(commands.Cog):
 			await ctx.send("Done. Saving")
 			await self.save_tv()
 			await ctx.send("Saved.")
-	
+
+	async def _pick_team(self,ctx,team):
+		em = discord.Embed()
+		em.color = 0x034f76
+		em.set_author(name = "LiveSoccerTV.com")
+
+		if team is not None:
+			matches = {i for i in self.bot.tv if team.lower() in i.lower()}
+			if not matches:
+				return await ctx.send(f"Could not find a matching team/league for {team}.")
+			if len(matches) == 1:
+				team = list(matches)[0]
+			else:
+				for i in matches:
+					if team == i:
+						result = i
+				else:
+					count = 0
+					matchdict = {}
+					for i in matches:
+						matchdict[str(count)] = i
+						count += 1
+					matchlist = [f'{i}: {matchdict[i]}' for i in matchdict]
+					strify = '\n'.join(matchlist)
+					msg = f"Please type matching id ```{strify}```"
+					def check(message):
+						if message.author.id == ctx.author.id and message.content in matchdict:
+							return True
+					delme = await ctx.send(msg)
+					result = await self.bot.wait_for("message",check=check,timeout=30)
+					team = matchdict[result.content]
+					await delme.delete()
+					await result.delete()
+			
+			em.url = self.bot.tv[team]
+			em.title = f"Televised Fixtures for {team}\n"
+		else:
+			em.url = "http://www.livesoccertv.com/schedules/"
+			em.title = f"Today's Televised Matches\n"
+		em.description = ""
+		return em
+
 	@commands.command()
+	@commands.is_owner()
 	async def tv(self,ctx,*,team:commands.clean_content = None):
 		""" Lookup next televised games for a team """
-		with ctx.typing():
-			if team and not team == "live":
-				matches = {i for i in self.bot.tv if team.lower() in i.lower()}
-				if not matches:
-					return await ctx.send(f"Could not find a matching team/league for {team}.")
-				elif len(matches) > 1:
-					for i in matches:
-						if team == i:
-							result = i
+		async with ctx.typing():
+			em = await self._pick_team(ctx,team)
+			tvlist = []
+			async with self.bot.session.get(em.url) as resp:
+				if resp.status != 200:
+					return await ctx.send(f"🚫 <{em.url}> returned {resp.status}")
+				tree = html.fromstring(await resp.text())
+				
+				matchcol = 3 if not team else 5
+				
+				for i in tree.xpath(".//table[@class='schedules'][1]//tr"):
+					# Discard finished games.
+					isdone = "".join(i.xpath('.//td[@class="livecell"]//span/@class')).strip()
+					if isdone in ["narrow ft","narrow repeat"]:
+						continue
+						
+					match = "".join(i.xpath(f'.//td[{matchcol}]//text()')).strip()
+					if not match:
+						continue
+					ml = i.xpath(f'.//td[{matchcol + 1}]//text()')
+					
+					try:
+						link = i.xpath(f'.//td[{matchcol + 1}]//a/@href')[-1]
+						link = f"http://www.livesoccertv.com/{link}"
+					except IndexError:
+						link = ""
+					
+					ml = ", ".join([x.strip() for x in ml if x != "nufcTV" and x.strip() != ""])
+					
+					if ml == []:
+						continue
+					
+					if isdone != "narrow live":
+						date = "".join(i.xpath('.//td[@class="datecell"]//span/text()')).strip()
+						time = "".join(i.xpath('.//td[@class="timecell"]//span/text()')).strip()
+						# Correct TimeZone offset.
+						try:
+							time = datetime.datetime.strptime(time,'%H:%M')+ datetime.timedelta(hours=5)
+							time = datetime.datetime.strftime(time,'%H:%M')
+							dt = f"{date} {time}"
+						except ValueError as e:
+							dt = ""
+					elif not team:
+						dt = i.xpath('.//td[@class="timecell"]//span/text()')[-1].strip()
+						if dt == "FT":
+							continue
+						if dt != "HT" and not ":" in dt:
+							dt = f"LIVE {dt}'"
 					else:
-						count = 0
-						matchdict = {}
-						for i in matches:
-							matchdict[str(count)] = i
-							count += 1
-						matchlist = [f'{i}: {matchdict[i]}' for i in matchdict]
-						strify = '\n'.join(matchlist)
-						msg = f"Please type matching id ```{strify}```"
-						def check(message):
-							if message.author.id == ctx.author.id and message.content in matchdict:
-								return True
-						delme = await ctx.send(msg)
-						result = await self.bot.wait_for("message",check=check,timeout=30)
-						team = matchdict[result.content]
-						await delme.delete()
-						await result.delete()
-				else:
-					team = list(matches)[0]
+						date = "".join(i.xpath('.//td[@class="datecell"]//span/text()')).strip()
+						time = "".join(i.xpath('.//td[@class="timecell"]//span/text()')).strip()
+						if date == datetime.datetime.now().strftime("%b %d"):
+							dt = time
+						else:
+							dt = date
+					
+					tvlist.append((f'`{dt}` [{match}]({link})'))
+			
+			if not tvlist:
+				return await ctx.send(f"Couldn't find any televised matches happening soon, check online at {em.url}")
+			dtn = datetime.datetime.now().strftime("%H:%M")
 
-				e = discord.Embed()
-				async with self.bot.session.get(self.bot.tv[team]) as resp:
-					if resp.status != 200:
-						await ctx.send(f"🚫 {resp.url} returned {resp.status}")
-						return
-					tree = html.fromstring(await resp.text())
-				tvlist = []
-				for i in tree.xpath(".//table[@class='schedules'][1]//tr"):
-					# Discard finished games.
-					isdone = "".join(i.xpath('.//td[@class="livecell"]//text()'))
-					if isdone in ["FT","Repeat"]:
-						continue
-					if "".join(i.xpath('.//td[@class="compcell"]/a/text()')) in "PL2":
-						continue
-					match = "".join(i.xpath('.//td[5]//text()')).strip()
-					if "vs" not in match:
-						continue
-					ml = i.xpath('.//td[6]//a/text()')
-					ml = [x for x in ml if x != "nufcTV" and x.strip() != ""]
-					if ml == []:
-						continue
-					else:
-						ml = ", ".join(ml)
-					date = "".join(i.xpath('.//td[@class="datecell"]//span/text()'))
-					date = date.strip()
-					time = "".join(i.xpath('.//td[@class="timecell"]//span/text()'))
-					time = time.strip()
-					# Correct TimeZone offset.
-					time = datetime.datetime.strptime(time,'%H:%M')+ datetime.timedelta(hours=5)
-					time = datetime.datetime.strftime(time,'%H:%M')
-					try:
-						link = i.xpath('.//td[6]//a/@href')[-1]
-					except IndexError:
-						continue
-					dt = f"`{date} {time}`"
-					lnk= f"http://www.livesoccertv.com/{link}"
-					tvlist.append((match,f'{dt} [{ml}]({lnk})'))
-				e = discord.Embed()
-				e.color = 0x034f76
-				e.title = "LiveSoccerTV.com"
-				if tvlist:
-					e.description = f"Upcoming Televised fixtures for {team}"
-				else:
-					return await ctx.send(f"Couldn't find any televised matches happening soon, check online at {self.bot.tv[team]}")
-				e.url = f"{resp.url}"
-				e.set_thumbnail(url='http://cdn.livesoccertv.com/images/logo55.png')
-				e.set_footer(text="UK times.")
-				for i,j in tvlist:
-					e.add_field(name=i,value=j,inline=False)
-				await ctx.send(embed=e)
-			else:
-				async with self.bot.session.get("http://www.livesoccertv.com/schedules/") as resp:
-					if resp.status != 200:
-						await ctx.send(f"🚫 {resp.url} returned {resp.status}")
-						return
-					tree = html.fromstring(await resp.text())
-					tvlist = []
-				for i in tree.xpath(".//table[@class='schedules'][1]//tr"):
-					# Discard finished games.
-					isdone = "".join(i.xpath('.//td[@class="livecell"]//text()'))
-					if isdone in ["FT","Repeat"]:
-						continue
-					match = "".join(i.xpath('.//td[3]//text()')).strip()
-					ml = i.xpath('.//td[4]//a/text()')
-					if ml == []:
-						continue
-					else:
-						ml = ", ".join(ml)
-					date = "".join(i.xpath('.//td[@class="datecell"]//span/text()'))
-					date = date.strip()
-					time = i.xpath('.//td[@class="timecell"]//span/text()')
-					time = time[-1].strip()
-					# Correct TimeZone offset.
-					if "FT" in time:
-						continue
-					if len(time) > 3:
-						time = datetime.datetime.strptime(time,'%H:%M')+ datetime.timedelta(hours=5)
-						time = datetime.datetime.strftime(time,'%H:%M')
-					else:
-						time = f"LIVE: {time}'"
-					try:
-						link = i.xpath('.//td[4]//a/@href')[-1]
-					except IndexError:
-						continue
-					dt = f"`{date} {time}`"
-					lnk= f"http://www.livesoccertv.com/{link}"
-					tvlist.append((match,f'{dt} [{ml}]({lnk})'))
-				e = discord.Embed()
-				e.color = 0x034f76
-				e.title = "LiveSoccerTV.com"
-				if tvlist:
-					e.description = f"Live Matches"
-				else:
-					return await ctx.send("Couldn't find any televised matches happening soon, check online at {self.bot.tv[team]}")
-				e.url = f"{resp.url}"
-				e.set_footer(text="UK times.")
-				chars = 0
-				for i,j in tvlist:
-					if len(j) + len(i) + chars < 5500:
-						e.add_field(name=i,value=j,inline=True)
-					chars += len(i) + len(j)
-				await ctx.send(embed=e)
+			em.set_footer(text=f"Time now: {dtn} Your Time:")
+			em.timestamp = datetime.datetime.now()
+			chars = 0
+			remain = len(tvlist)
+			for x in tvlist:
+				if len(x) + + chars < 2000:
+					em.description += x + "\n"
+					remain -= 1
+				chars += len(x) + 5
+				
+			if remain:
+				em.description += f"\n *and {remain} more...*"
+			await ctx.send(embed=em)
 
 
 def setup(bot):
