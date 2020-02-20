a24d30c78b468f20ce462e6039f4a76e3f5eacc3
Painezor <Paine.exe@gmail.com>
2020-02-20 12:34:41 +0000

    Handle footers, with preserve_footer KWARG. Quotes pages now go to a field.

--- ext/utils/embed_utils.py
+++ ext/utils/embed_utils.py
@@ -7,7 +7,12 @@ from PIL import UnidentifiedImageError
 from colorthief import ColorThief
 
 
-async def get_colour(url):
+ICON_URL = "http://pix.iemoji.com/twit33/0056.png"
+
+async def get_colour(url=None):
+    if url is None:
+        return discord.Colour.blurple()
+    
     async with aiohttp.ClientSession() as cs:
         async with cs.get(url) as resp:
             r = await resp.read()
@@ -20,11 +25,32 @@ async def get_colour(url):
                 return discord.Colour.blurple()
 
 
-async def paginate(ctx, embeds, id_dict=None):
+async def paginate(ctx, embeds, preserve_footer=False, id_dict=None):
+    count = 0
+    for e in embeds:
+        count += 1
+        page_line = f"{ctx.author}: Page {count} of {len(embeds)}"
+        if preserve_footer:
+            e.add_field(name="Page", value=page_line)
+        else:
+            e.set_footer(icon_url=ICON_URL, text=page_line)
+    
     # Paginator
     page = 0
     try:
-        m = await ctx.send(embed=embeds[page])
+        if ctx.me.permissions_in(ctx.channel).add_reactions:
+            if not ctx.me.permissions_in(ctx.channel).manage_messages :
+                if ctx.guild is None:
+                    warn = "I can't remove your reactions in DMs, so you'll have to click twice."
+                else:
+                    warn = "I don't have manage_messages permissions, so you'll have to click twice."
+                m = await ctx.send(warn, embed=embeds[page])
+            else:
+                m = await ctx.send(embed=embeds[page])
+        else:
+            if ctx.guild is not None:
+                warn = "I don't have add_reaction permissions so I can only show you the first page of results."
+                return await ctx.send(warn, embed=embeds[page])
     except IndexError:
         return await ctx.send("Couldn't find anything.")
     
@@ -36,7 +62,7 @@ async def paginate(ctx, embeds, id_dict=None):
         ctx.bot.loop.create_task(m.add_reaction("▶"))  # next
         if len(embeds) > 2:
             ctx.bot.loop.create_task(m.add_reaction("⏭"))  # last
-    ctx.bot.loop.create_task(m.add_reaction("🚫"))  # Delete
+        ctx.bot.loop.create_task(m.add_reaction('🚫'))
     
     def react_check(r, u):
         if r.message.id == m.id and u.id == ctx.author.id:
@@ -47,8 +73,6 @@ async def paginate(ctx, embeds, id_dict=None):
         return message.content in idd
     
     while not ctx.bot.is_closed():
-        remind_once = False
-        
         # If we're passing an id_dict, we want to get the user's chosen result from the dict.
         # But we always want to be able to change page, or cancel the paginator.
         if id_dict is not None:
@@ -59,11 +83,19 @@ async def paginate(ctx, embeds, id_dict=None):
             try:
                 result = finished.pop().result()
             except KeyError:  # pop from empty set.
+                try:
+                    await m.clear_reactions()
+                except discord.Forbidden:
+                    pass
                 return None
         else:
             try:
                 result = await ctx.bot.wait_for("reaction_add", check=react_check, timeout=60)
             except asyncio.TimeoutError:
+                try:
+                    await m.clear_reactions()
+                except discord.Forbidden:
+                    pass
                 return None
             else:
                 pending = []
@@ -90,10 +122,7 @@ async def paginate(ctx, embeds, id_dict=None):
             try:
                 await m.remove_reaction(result[0].emoji, ctx.author)
             except discord.Forbidden:
-                if not remind_once:
-                    await ctx.send("I don't have the manage_reactions permissions to remove your reactions,"
-                                   " so you'll need to click twice to change page.")
-                    remind_once = True
+                pass  # swallow this error.
             await m.edit(embed=embeds[page])
         else:
             # We actually return something.
